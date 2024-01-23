a53fca9c25e7e8fad17e9e884c997a481d48e9cf
VihangaIndeewra <indeewravihnga@gmail.com>
2024-01-23 10:43:11 +0530

    Added prediction pipeline for application backend

--- main.py
+++ main.py
@@ -1,15 +1,59 @@
-from typing import Union
 
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Form, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse , RedirectResponse
+from fastapi.staticfiles import StaticFiles
+
+import logging
+from pipeline import preprocessing ,vectorizer,get_prediction
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+templates = Jinja2Templates(directory="templates")
 
+logging.info('FastAPI server started')
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+data = dict()
+reviews = []
+positive = 0
+negative = 0
+
+@app.get("/" , response_class=HTMLResponse)
+def read_root(request:Request):
+    data['reviews'] = reviews
+    data['positive'] = positive
+    data['negative'] = negative
+
+    logging.info('========== Open home page ============')
+
+    return templates.TemplateResponse("index.html",{"request": request, "data": data})
+
+@app.post("/")
+async def submit(request: Request, text: str=Form()):
+    logging.info(f'Text : {text}')
+
+    preprocessedText=preprocessing(text)
+    logging.info(f'Preprocessed Text : {preprocessedText}')
+    
+    vectorizedText=vectorizer(preprocessedText)
+    logging.info(f'Vectorized Text : {vectorizedText}')
+
+    prediction=get_prediction(vectorizedText)
+    logging.info(f'Prediction : {prediction}')
+
+    
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+
+    reviews.insert(0, text)
+            
+    return RedirectResponse("/", status_code=303)
+ 
