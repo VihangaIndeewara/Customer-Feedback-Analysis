e47918da52e839d79f3b40713de9f437cb0b22da
VihangaIndeewra <indeewravihnga@gmail.com>
2024-01-29 09:15:04 +0530

    Added Frontend

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse , RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
-import logging
 from pipeline import preprocessing ,vectorizer,get_prediction
 
 app = FastAPI()
@@ -14,7 +13,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-logging.info('FastAPI server started')
+
 
 data = dict()
 reviews = []
@@ -27,24 +26,13 @@ def read_root(request:Request):
     data['positive'] = positive
     data['negative'] = negative
 
-    logging.info('========== Open home page ============')
-
     return templates.TemplateResponse("index.html",{"request": request, "data": data})
 
 @app.post("/")
 async def submit(request: Request, text: str=Form()):
-    logging.info(f'Text : {text}')
-
-    preprocessedText=preprocessing(text)
-    logging.info(f'Preprocessed Text : {preprocessedText}')
-    
+    preprocessedText=preprocessing(text)  
     vectorizedText=vectorizer(preprocessedText)
-    logging.info(f'Vectorized Text : {vectorizedText}')
-
-    prediction=get_prediction(vectorizedText)
-    logging.info(f'Prediction : {prediction}')
-
-    
+    prediction=get_prediction(vectorizedText)   
 
     if prediction == 'negative':
         global negative
